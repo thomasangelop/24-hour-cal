ce6ce352fd30d0794eb25a381f67c5dede7de999
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-17 16:27:16 -0600

    preparing to create heroku

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>24 Hour Cal</h1>
+          <h3>React website and app coming soon!</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
