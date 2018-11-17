2bbf33175f59b7b22c706f60d627c43f9e77174f
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-17 16:33:38 -0600

    build website coming soon react app

---
+++ build/precache-manifest.d4f1c1998c371920a165685c65a70500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27d27e7c17f63622f622",
+    "url": "/static/js/main.27d27e7c.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "27d27e7c17f63622f622",
+    "url": "/static/css/main.fea19602.chunk.css"
+  },
+  {
+    "revision": "6ec0fd313a260a65c062deea20176bd6",
+    "url": "/index.html"
+  }
+];
