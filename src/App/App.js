3e4cb8dbe73adda301b982fe6eaa8179866db31c
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-19 11:29:15 -0600

    deleted calednar work from sunday off master

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import MenuBar from '../MenuBar/MenuBar';
 import Home from '../Home/Home';
 
+
 class App extends Component {
   render() {
     return (
       <div>
         <MenuBar />
         <Home />
+        <div>
+        </div>
       </div>
     );
   }
